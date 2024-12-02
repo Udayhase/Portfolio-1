2371a8cbb6e1defad91d4092f285b971d8e828d8
Uday Hase <haseuday77@gmail.com>
2024-12-02 17:07:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("learningButton").addEventListener("click", function() {
+  alert("User is  still learning. 😊");
+});
+
+
+document.getElementById("wp").addEventListener("click", function() {
+  alert("sorry😢, please try another link😊");
+});
